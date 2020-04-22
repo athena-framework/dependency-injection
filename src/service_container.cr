cbfe7cbdeaa67e72eacd8c1a0203f7197cad35cb
George Dietrich <george@dietrich.app>
2020-04-21 20:30:39 -0400

    Support resolving named arguments via string ID or type
    Support resolving positional arguments via type
    Fix creating public getter for aliased services

--- src/service_container.cr
+++ src/service_container.cr
@@ -133,72 +133,12 @@ struct Athena::DependencyInjection::ServiceContainer
                 tagged_services = tagged_services.sort_by { |item| -(item[1][:priority] || 0) }
 
                 %(#{tagged_services.map(&.first)} of Union(#{initializer_args[idx].restriction.resolve.type_vars.splat})).id
-              else
-                arg
-              end
-            end
-          %}
-        {% else %}
-          # Otherwise, try and auto resolve the arguments
-          {%
-            arguments = initializer_args.map_with_index do |arg, idx|
-              # Check if an explicit value was passed for this arg
-              if service_ann && service_ann.named_args.keys.includes? "_#{arg.name}".id
-                named_arg = service_ann.named_args["_#{arg.name}"]
-
-                if named_arg.is_a?(ArrayLiteral)
-                  inner_args = arg.map_with_index do |arr_arg, arr_idx|
-                    inner_initializer = service.methods.find(&.annotation(ADI::Inject)) || service.methods.find(&.name.==("initialize"))
-                    inner_initializer_args = (i = initializer) ? i.args : [] of Nil
-
-                    if arr_arg.is_a?(ArrayLiteral)
-                      arr_arg.raise "More than two level nested arrays are not currently supported."
-                    elsif arr_arg.is_a?(StringLiteral) && arr_arg.starts_with?("@?")
-                      s_id = arr_arg[2..-1]
-
-                      (s = service_hash[s_id]) ? s_id.id : nil
-                    elsif arr_arg.is_a?(StringLiteral) && arr_arg.starts_with?('@')
-                      service_name = arr_arg[1..-1]
-                      raise "Failed to resolve service '#{service_name.id}'.  Does it exist?" unless service_hash[service_name]
-                      service_name.id
-                    else
-                      arr_arg
-                    end
-                  end
-
-                  %(#{inner_args} of Union(#{initializer_args[idx].restriction.resolve.type_vars.splat})).id
-                elsif named_arg.is_a?(StringLiteral) && named_arg.starts_with?("@?")
-                  s_id = named_arg[2..-1]
-
-                  (s = service_hash[s_id]) ? s_id.id : nil
-                elsif named_arg.is_a?(StringLiteral) && named_arg.starts_with?('@')
-                  service_name = named_arg[1..-1]
-                  raise "Failed to resolve service '#{service_name.id}'.  Does it exist?" unless service_hash[service_name]
-                  service_name.id
-                elsif named_arg.is_a?(StringLiteral) && named_arg.starts_with?('!')
-                  tagged_services = [] of Nil
-
-                  # Build an array of services with the given tag,
-                  # along with the tag metadata
-                  service_hash.each do |s_id, metadata|
-                    if t = metadata[:tags].find { |tag| tag[:name] == named_arg[1..-1] }
-                      tagged_services << {s_id.id, t}
-                    end
-                  end
-
-                  # Sort based on tag priority.  Services without a priority will be last in order of definition
-                  tagged_services = tagged_services.sort_by { |item| -(item[1][:priority] || 0) }
-
-                  %(#{tagged_services.map(&.first)} of Union(#{initializer_args[idx].restriction.resolve.type_vars.splat})).id
-                else
-                  named_arg
-                end
-              else
+              elsif arg.is_a?(Path)
                 resolved_services = [] of Nil
 
                 # Otherwise resolve possible services based on type
                 service_hash.each do |s_id, metadata|
-                  if (type = arg.restriction.resolve?) && metadata[:type] <= type
+                  if (type = arg.resolve?) && metadata[:type] <= type
                     resolved_services << s_id
                   end
                 end
@@ -229,6 +169,174 @@ struct Athena::DependencyInjection::ServiceContainer
                     arg.raise "Could not auto resolve argument '#{arg}'."
                   end
                 end
+              else
+                arg
+              end
+            end
+          %}
+        {% else %}
+          # Otherwise, try and auto resolve the arguments
+          {%
+            arguments = initializer_args.map_with_index do |initializer_arg, idx|
+              # Check if an explicit value was passed for this initializer_arg
+              if service_ann && service_ann.named_args.keys.includes? "_#{initializer_arg.name}".id
+                named_arg = service_ann.named_args["_#{initializer_arg.name}"]
+
+                if named_arg.is_a?(ArrayLiteral)
+                  inner_args = initializer_arg.map_with_index do |arr_arg, arr_idx|
+                    inner_initializer = service.methods.find(&.annotation(ADI::Inject)) || service.methods.find(&.name.==("initialize"))
+                    inner_initializer_args = (i = initializer) ? i.args : [] of Nil
+
+                    if arr_arg.is_a?(ArrayLiteral)
+                      arr_arg.raise "More than two level nested arrays are not currently supported."
+                    elsif arr_arg.is_a?(StringLiteral) && arr_arg.starts_with?("@?")
+                      s_id = arr_arg[2..-1]
+
+                      (s = service_hash[s_id]) ? s_id.id : nil
+                    elsif arr_arg.is_a?(StringLiteral) && arr_arg.starts_with?('@')
+                      service_name = arr_arg[1..-1]
+                      raise "Failed to resolve service '#{service_name.id}'.  Does it exist?" unless service_hash[service_name]
+                      service_name.id
+                    elsif arr_arg.is_a?(Path)
+                      resolved_services = [] of Nil
+
+                      # Otherwise resolve possible services based on type
+                      service_hash.each do |s_id, metadata|
+                        if (type = arr_arg.resolve?) && metadata[:type] <= type
+                          resolved_services << s_id
+                        end
+                      end
+
+                      # If no services could be resolved
+                      if resolved_services.size == 0
+                        # Return a default value if any
+                        unless initializer_arg.default_value.is_a? Nop
+                          initializer_arg.default_value
+                        else
+                          # otherwise raise an exception
+                          initializer_arg.raise "Could not auto resolve argument '#{initializer_arg}'.  Does it exist?"
+                        end
+                      elsif resolved_services.size == 1
+                        # If only one was matched, return it
+                        resolved_services[0].id
+                      else
+                        # Otherwise fallback on the argument's name as well
+                        if resolved_service = resolved_services.find(&.==(initializer_arg.name))
+                          resolved_service.id
+                          # If no service with that name could be resolved,
+                          # check the alias map for the restriction
+                        elsif aliased_service = alias_hash[initializer_arg.restriction.resolve]
+                          # If one is found returned the aliased service
+                          aliased_service.id
+                        else
+                          # Otherwise raise an exception
+                          initializer_arg.raise "Could not auto resolve argument '#{initializer_arg}'."
+                        end
+                      end
+                    else
+                      arr_arg
+                    end
+                  end
+
+                  %(#{inner_args} of Union(#{initializer_args[idx].restriction.resolve.type_vars.splat})).id
+                elsif named_arg.is_a?(StringLiteral) && named_arg.starts_with?("@?")
+                  s_id = named_arg[2..-1]
+
+                  (s = service_hash[s_id]) ? s_id.id : nil
+                elsif named_arg.is_a?(StringLiteral) && named_arg.starts_with?('@')
+                  service_name = named_arg[1..-1]
+                  raise "Failed to resolve service '#{service_name.id}'.  Does it exist?" unless service_hash[service_name]
+                  service_name.id
+                elsif named_arg.is_a?(StringLiteral) && named_arg.starts_with?('!')
+                  tagged_services = [] of Nil
+
+                  # Build an array of services with the given tag,
+                  # along with the tag metadata
+                  service_hash.each do |s_id, metadata|
+                    if t = metadata[:tags].find { |tag| tag[:name] == named_arg[1..-1] }
+                      tagged_services << {s_id.id, t}
+                    end
+                  end
+
+                  # Sort based on tag priority.  Services without a priority will be last in order of definition
+                  tagged_services = tagged_services.sort_by { |item| -(item[1][:priority] || 0) }
+
+                  %(#{tagged_services.map(&.first)} of Union(#{initializer_args[idx].restriction.resolve.type_vars.splat})).id
+                elsif named_arg.is_a?(Path)
+                  resolved_services = [] of Nil
+
+                  # Otherwise resolve possible services based on type
+                  service_hash.each do |s_id, metadata|
+                    if (type = named_arg.resolve?) && metadata[:type] <= type
+                      resolved_services << s_id
+                    end
+                  end
+
+                  # If no services could be resolved
+                  if resolved_services.size == 0
+                    # Return a default value if any
+                    unless initializer_arg.default_value.is_a? Nop
+                      initializer_arg.default_value
+                    else
+                      # otherwise raise an exception
+                      initializer_arg.raise "Could not auto resolve argument '#{initializer_arg}' with explicit argument '#{named_arg}'.  Does it exist?"
+                    end
+                  elsif resolved_services.size == 1
+                    # If only one was matched, return it
+                    resolved_services[0].id
+                  else
+                    # Otherwise fallback on the argument's name as well
+                    if resolved_service = resolved_services.find(&.==(initializer_arg.name))
+                      resolved_service.id
+                      # If no service with that name could be resolved,
+                      # check the alias map for the restriction
+                    elsif aliased_service = alias_hash[initializer_arg.restriction.resolve]
+                      # If one is found returned the aliased service
+                      aliased_service.id
+                    else
+                      # Otherwise raise an exception
+                      initializer_arg.raise "Could not auto resolve argument '#{initializer_arg}' with explicit argument '#{named_arg}'."
+                    end
+                  end
+                else
+                  named_arg
+                end
+              else
+                resolved_services = [] of Nil
+
+                # Otherwise resolve possible services based on type
+                service_hash.each do |s_id, metadata|
+                  if (type = initializer_arg.restriction.resolve?) && metadata[:type] <= type
+                    resolved_services << s_id
+                  end
+                end
+
+                # If no services could be resolved
+                if resolved_services.size == 0
+                  # Return a default value if any
+                  unless initializer_arg.default_value.is_a? Nop
+                    initializer_arg.default_value
+                  else
+                    # otherwise raise an exception
+                    initializer_arg.raise "Could not auto resolve argument '#{initializer_arg}'.  Does it exist?"
+                  end
+                elsif resolved_services.size == 1
+                  # If only one was matched, return it
+                  resolved_services[0].id
+                else
+                  # Otherwise fallback on the argument's name as well
+                  if resolved_service = resolved_services.find(&.==(initializer_arg.name))
+                    resolved_service.id
+                    # If no service with that name could be resolved,
+                    # check the alias map for the restriction
+                  elsif aliased_service = alias_hash[initializer_arg.restriction.resolve]
+                    # If one is found returned the aliased service
+                    aliased_service.id
+                  else
+                    # Otherwise raise an exception
+                    initializer_arg.raise "Could not auto resolve argument '#{initializer_arg}'."
+                  end
+                end
               end
             end
           %}
@@ -252,13 +360,13 @@ struct Athena::DependencyInjection::ServiceContainer
 
       # Define getters for aliased service, if the alias is public, make the getter public and also define a type based getter
       {% for service_type, service_id in alias_hash %}
+        {% metadata = service_hash[service_id] %}
+
         {% type = metadata[:generics].empty? ? metadata[:type] : "#{metadata[:type].name(generic_args: false)}(#{metadata[:generics].splat})".id %}
 
-        {% service = service_hash[service_id] %}
+        {% if metadata[:public_alias] != true %}private{% end %} def {{service_type.name.gsub(/::/, "_").underscore.id}} : {{type}}; {{service_id.id}}; end
 
-        {% if service[:public_alias] != true %}private{% end %} def {{service_type.name.gsub(/::/, "_").underscore.id}} : {{type}}; {{service_id.id}}; end
-
-        {% if service[:public_alias] %}
+        {% if metadata[:public_alias] %}
           def get(service : {{type}}.class) : {{type}}
             {{service_id.id}}
           end
@@ -269,13 +377,6 @@ struct Athena::DependencyInjection::ServiceContainer
       def initialize
         # Work around for https://github.com/crystal-lang/crystal/issues/7975
         {{@type}}
-
-        # Initialize non lazy services
-        {% for service_id, metadata in service_hash %}
-          {% unless metadata[:lazy] == true %}
-            @{{service_id.id}} = {{service_id.id}}
-          {% end %}
-        {% end %}
       end
     {% end %}
   end
